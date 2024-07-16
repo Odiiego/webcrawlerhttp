913983869769a5f5a89102a1b6e7ac1f217a119f
Diego <odiegodev@gmail.com>
2024-07-16 04:31:20 -0300

    feat: add printReport function

--- report.js
+++ report.js
@@ -1,6 +1,20 @@
+function printReport(pages) {
+  console.log(`--------------------\n       REPORT\n--------------------`);
+  const sortedPages = sortPages(pages);
+  const strLen = String(sortedPages[0][1]).length;
+  for (const sortedPage of sortedPages) {
+    const url = sortedPage[0];
+    const hits = sortedPage[1];
+    console.log(
+      `Found ${String(hits).padStart(strLen, ' ')} links to page: ${url}`,
+    );
+  }
+  console.log(`--------------------\n   END REPORT\n--------------------`);
+}
+
 function sortPages(pages) {
   const pagesArr = Object.entries(pages);
   return pagesArr.sort((a, b) => b[1] - a[1]);
 }
 
-module.exports = { sortPages };
+module.exports = { printReport, sortPages };
